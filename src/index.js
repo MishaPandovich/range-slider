18500ce29650f9d5e3491644e9cb0fd7561ec073
Виктория <Ovcharka@gmail.com>
2019-06-22 21:13:25 +0300

    проверка файла

--- src/index.js
+++ src/index.js
@@ -3,29 +3,27 @@ import Method from './addFacade.js';
 
 
 ;(function($) {
-	var defaults = {
-		vertSlider : false,
-		rangeSlider: true,
-		posThumbLeft: 10,
-		posThumbRigth: 50
-	}
+  var defaults = {
+    vertSlider : false,
+    rangeSlider: true,
+    posThumbLeft: 10,
+    posThumbRigth: 50
+  }
 
-	const method = new Method();
+  const method = new Method();
 
-	$.fn.slider = function(options) {
-		var config = $.extend({}, defaults, options);
-		var parentEl = this[0];
+  $.fn.slider = function(options) {
+    var config = $.extend({}, defaults, options);
+    var parentEl = this[0];
 
-		method.createSlider(config.rangeSlider,
-							config.vertSlider,
-							parentEl,
-							config.posThumbLeft,
-							config.posThumbRigth);
+    method.createSlider(config.rangeSlider,
+                        config.vertSlider,
+                        parentEl,
+                        config.posThumbLeft,
+                        config.posThumbRigth);
 
-		(config.vertSlider) ? method.initVertSlider(config.rangeSlider) : method.initSlider(config.rangeSlider);	
+    (config.vertSlider) ? method.initVertSlider(config.rangeSlider) : method.initSlider(config.rangeSlider);  
 
-		return this;
-	};
+    return this;
+  };
 })(jQuery);
-
-
